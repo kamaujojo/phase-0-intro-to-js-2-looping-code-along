3df4d5c37a29f90ad44d8ae69a22ece565db12df
joan <joan.kamau@student.moringaschool.com>
2024-07-20 23:36:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    if (!Array.isArray(names)) {
+      return 'Please provide an array of names.';
+    }
+    names.forEach(name => {
+      let message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    });
+    return thankYouMessages;
+  }
+// test the function
+//write a function, countDown, that takes in any positive integer and,
+// starting from that number, counts down to zero using console.log()
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+    return int;
+}
+
+const nums = countDown(10);
+console.log(nums);
